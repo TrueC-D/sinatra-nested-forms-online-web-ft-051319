af7aff2526e759d0156e1e2c496fe0278f4f5ebc
Alethea Cary <aletheacary@gmail.com>
2019-07-05 13:55:57 +0000

    Done.

--- app.rb
+++ app.rb
@@ -12,9 +12,15 @@ module FormsLab
     end
     
     post '/pirates' do 
-      @pirate = params[:pirate]
-      @ship1 = params[:pirate][:ships][0]
-      @ship2 = params[:pirate][:ships][1]
+      @pirate_name = params[:pirate][:name]
+      @pirate_weight = params[:pirate][:weight]
+      @pirate_height = params[:pirate][:height]
+      @ship1_name = params[:pirate][:ships][0][:name]
+      @ship1_type = params[:pirate][:ships][0][:type]
+      @ship1_booty = params[:pirate][:ships][0][:booty]
+      @ship2_name = params[:pirate][:ships][1][:name]
+      @ship2_type = params[:pirate][:ships][1][:type]
+      @ship2_booty = params[:pirate][:ships][1][:booty]
       
       erb :'pirates/show'
     end
