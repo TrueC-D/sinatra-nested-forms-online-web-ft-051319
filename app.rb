e7ad680fc20b6a37c58a6b2dd7da59bc85140c86
Alethea Cary <aletheacary@gmail.com>
2019-07-05 13:42:57 +0000

    Done.

--- app.rb
+++ app.rb
@@ -7,11 +7,14 @@ module FormsLab
     
     get '/new' do
       # set :root, File.join(File.dirname(views/pirates), '..')
-      erb :new
+      erb :'pirates/new'
       
     end
     
     post '/pirates' do 
+      @pirate = params[:pirate]
+      @ship1 = params[:pirate][:ships][0]
+      @ship2 = params[:pirate][:ships][1]
       
       erb :show
     end
